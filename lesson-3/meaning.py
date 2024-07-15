23db42dfaad52171a1e1d0bdd40cc3f83d87b68f
Jess Bayman <justjessb@gmail.com>
2024-07-15 13:48:05 +1200

    Look at conditional statements and ternary operators

---
+++ lesson-3/meaning.py
@@ -0,0 +1,11 @@
+meaning = 42
+print('')
+
+# iF ELSE STATEMENT EXAMPLE
+if meaning > 10: 
+  print('Right on!')
+else: 
+  print('Not today')
+
+# TERNARY OPERATOR EXAMPLE
+print('Right on!') if meaning > 10 else print('Not today')
